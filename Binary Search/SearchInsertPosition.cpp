2fa686e43b81a9d6fa587ea19d289489ec8e3fbb
arvinder004 <asdhoul004@gmail.com>
2024-11-12 06:33:05 +0530

    search insert position

---
+++ Binary Search/SearchInsertPosition.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int searchInsertPosition(int arr[], int n, int x){
+    int low = 0, high = n-1, lb = n;
+
+    while (low <= high)
+    {
+        int mid = low+high/2;
+
+        if(arr[mid] >= x){
+            lb = mid;
+            high = mid - 1;
+        }
+        else {
+            low = mid + 1;
+        }
+    }
+    return lb;
+}
+
+int main (){
+    int arr[] = {2,3,5,7,8,8,11,11,13,15,24};
+    int n = sizeof(arr)/sizeof(arr[0]);
+
+    int index = searchInsertPosition(arr, n, 8);
+
+    cout << index << " " << arr[index] << endl;
+}
